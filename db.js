28bc3f1e6ce0472eeedcd3f49bce5bbfc0347c91
Marie Nabors <marienabors@thefirstacademy.org>
2018-03-17 09:10:52 -0400

    added css to login

--- db.js
+++ db.js
@@ -1,13 +1,26 @@
 var mysql = require('mysql')
 var connection = mysql.createConnection({
-  host: iwqrvsv8e5fz4uni.cbetxkdyhwsb.us-east-1.rds.amazonaws.com,
-  user: q148vjr9uxbbomyo,
-  password: mcubyi2fskglaz1z,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   bootcamp: process.env.DB_BOOTCAMP,
   database : process.env.DB_NAME,
   socketPath: '/tmp/mysql.sock'
 })
-
+var connection;
+if(process.env.JAWSDB_URL) {
+  //Heroku deployment
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  //local host
+    connection = mysql.createConnection({
+        root: 3000,
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "db_name",
+    });
+};
 connection.connect();
 
 connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
